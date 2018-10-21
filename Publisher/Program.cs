095320ccc05ea5f6812e8f1be2d36e47bcce6f25
benoitgoethals <benoitgoethals@outlook.com>
2018-10-21 18:34:24 +0200

    init

---
+++ Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=192.168.0.163;username=test;password=test"))
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    bus.Send("queke",new TextMessage() { Text="dfsf"+i });
+                    bus.PublishAsync(new TextMessage()
+                    {
+                        Text = "sdfsdfs" + i
+                    }).ContinueWith(task =>
+                    {
+                        if(task.IsCompleted && !task.IsFaulted)
+                        {
+                            Console.WriteLine("task Compled");
+                        }
+                    });
+                }
+                   
+                }
+            Console.ReadLine();
+            }
+        }
+    }
---
+++ Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=192.168.0.163;username=test;password=test"))
+            {
+                bus.Receive("queke", x => x.Add<TextMessage>(m => { Console.WriteLine(m.Text);
+                 
+                }));
+                bus.Subscribe<TextMessage>("test", HandleTextMessage);
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static void Reiver(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+        }
+    }
+}
